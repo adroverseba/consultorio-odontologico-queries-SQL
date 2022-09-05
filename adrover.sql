ef84f2915b68629cd197320708c2ca106e666522
adroverseba <adroversebastian.sa@gmail.com>
2022-09-04 21:58:45 -0300

    vistas

--- adrover.sql
+++ adrover.sql
@@ -83,3 +83,53 @@ CREATE TABLE IF NOT EXISTS equipos(
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+--                                  creacion de VISTAS
+
+
+-- genero la vista de los costos de tratamientos, el IVA de los mismos y su costo total
+CREATE VIEW v_costo_tratamientos AS 
+SELECT *, ROUND(costo*0.21,2) IVA, SUM(ROUND(costo*0.21,2)+costo) TOTAL
+FROM tratamientos 
+GROUP BY id_tratamiento
+ORDER BY costo DESC
+;
+
+SELECT * FROM v_costo_tratamientos;
+
+
+-- vista de equipos con fecha de mantenimiento mayores a un anho
+CREATE OR REPLACE VIEW v_fecha_mantenimiento_viejas AS
+SELECT * FROM equipos
+WHERE DATEDIFF(NOW(),fecha_mantenimiento) > 365 
+;
+
+SELECT * v_fecha_mantenimiento_viejas;
+
+
+
+-- vista con datos del cliente, tratamiento, costos y n de legajo del odontologo
+CREATE OR REPLACE VIEW v_cliente_tratamientos AS 
+SELECT t.id_tratamiento, t.nombre as tratamiento, t.costo, t.legajo as odontologo_legajo, a.nombre as paciente, a.telefono
+FROM tratamientos t  
+JOIN 
+(
+	SELECT p.nombre, p.telefono ,c.id_tratamiento
+	FROM consultas c
+	JOIN pacientes p ON (c.id_paciente=p.id_paciente)
+) a ON t.id_tratamiento= a.id_tratamiento
+;
+
+-- vista que contiene calculo de edad del paciente y condicional CASE que 
+-- define el tipo de cliente segun su edad 
+CREATE OR REPLACE VIEW v_edad_tipo_cliente AS
+SELECT *, ROUND(DATEDIFF(NOW(), p.fecha_nacimiento)/365,0) as edad,
+CASE
+	WHEN ROUND(DATEDIFF(NOW(), p.fecha_nacimiento)/365,0) > 50 THEN 'Adulto Mayor'
+    WHEN ROUND(DATEDIFF(NOW(), p.fecha_nacimiento)/365,0) >= 18 THEN 'Adulto' 
+    ELSE 'Infantes'
+END as tipo_cliente
+FROM pacientes p
+;
+
